3b346f69e66f396e9bede431e90db5b8f61ae243
shahzaib1007 <khan.shahzaib1007@gmail.com>
2025-06-02 17:58:14 -0700

    added tiff preprocessing part in the run_sdrips

--- src/sdrips/run_sdrips.py
+++ src/sdrips/run_sdrips.py
@@ -19,12 +19,19 @@ from typing import Tuple, Dict, List
 
 from sdrips.utils.clean import clear_data
 from sdrips.utils.make_directory import make_directory
-from sdrips.evapotranspiration import process_cmd_area_parallel 
+from sdrips.evapotranspiration import (
+    process_cmd_area_parallel,
+    get_cmd_area_list)
 from sdrips.utils.utils import load_yaml_config
 from sdrips.utils.logging_utils import (
     setup_logger_with_queue,
     worker_logger_setup,
     worker_init
+)
+from sdrips.tiff_preprocess import (
+    unzip_tiffs, 
+    convert_tiffs, 
+    converting_to_eto
 )
 
 
@@ -41,6 +48,7 @@ def run_et_for_ca(save_data_loc: str, log_queue) -> Dict[str, List[float]]:
         logger.info("Starting ET estimation...")
 
         penman_mean_values,sebal_mean_values,irr_mean_values = process_cmd_area_parallel(logger, log_queue)
+        # cmd_area_list = process_cmd_area_parallel(save_data_loc, log_queue)
     except Exception as e:
         logger.exception("Unhandled exception in main process")
 
@@ -76,6 +84,7 @@ def main():
     run_week = config['Date_Running']['run_week']
     clear_condition = config['Clean_Directory'].get('clear_directory_condition', False)
     run_et = config['Run_ET_Estimation'].get('et_estimation', False)
+    cmd_area_list = get_cmd_area_list()
 
     log_queue, queue_listener, log_file_path = setup_logger_with_queue(save_data_loc)
     logger = logging.getLogger()
@@ -100,7 +109,10 @@ def main():
 
         if run_et:
             logger.info("Running ET module for all command areas...")
-            run_et_for_ca(save_data_loc, log_queue)
+            cmd_area_list = run_et_for_ca(save_data_loc, log_queue)
+            unzip_tiffs(save_data_loc, run_week)
+            convert_tiffs(save_data_loc, run_week)
+            converting_to_eto(cmd_area_list, save_data_loc, run_week)
 
     except Exception as e:
         logger.exception("Unhandled exception during sDRIPS execution")
