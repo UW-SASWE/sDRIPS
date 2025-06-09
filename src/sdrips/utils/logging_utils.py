86d0a2d662b541e808dc61c4159d3b3d9ce60a7f
shahzaib1007 <khan.shahzaib1007@gmail.com>
2025-06-08 19:02:34 -0700

    updated setup logger to take config path as the only input

--- src/sdrips/utils/logging_utils.py
+++ src/sdrips/utils/logging_utils.py
@@ -5,9 +5,11 @@ import multiprocessing
 import os
 import datetime
 from ruamel.yaml import YAML
-
 from multiprocessing.queues import Queue
 from typing import Tuple, Dict, List
+
+from sdrips.utils.utils import load_yaml_config
+
 
 class ExcludeModulesFilter(logging.Filter):
     def __init__(self, excluded_modules):
@@ -17,7 +19,18 @@ class ExcludeModulesFilter(logging.Filter):
     def filter(self, record):
         return not any(record.pathname.endswith(mod) for mod in self.excluded_modules)
     
-def setup_logger_with_queue(save_data_loc: str) -> Tuple[Queue, QueueListener, str]:
+def setup_logger_with_queue(config_path: str) -> Tuple[Queue, QueueListener, str]:
+    """
+    Setup logging configuration with a multiprocessing queue for log messages.
+    This function initializes a logger that writes to a file, excluding logs from specific modules.
+    Args:
+        config_path (str): Path to the main configuration file.
+
+    Returns:
+        Tuple[Queue, QueueListener, str]: A tuple containing the log queue, queue listener, and the log file path.
+    """
+    script_config = load_yaml_config(config_path)
+    save_data_loc = script_config['Save_Data_Location']['save_data_loc']
     os.makedirs(f'{save_data_loc}/logs/', exist_ok=True)
     dt_fmt = '%Y%m%d_%H%M%S'
     log_file = os.path.abspath(f'{save_data_loc}/logs/{datetime.datetime.today().strftime(dt_fmt)}.log')
@@ -31,7 +44,7 @@ def setup_logger_with_queue(save_data_loc: str) -> Tuple[Queue, QueueListener, s
     excluded_modules = ['discovery.py', 'connectionpool.py', 'env.py', 
     '__init__.py', 'warp.py', 'mask.py', 
     'utils.py', 'features.py', 'collection.py',
-    'collection.py']
+    'collection.py', 'geodataframe.py']
     file_handler.addFilter(ExcludeModulesFilter(excluded_modules))
 
     queue_listener = QueueListener(log_queue, file_handler)
@@ -39,7 +52,18 @@ def setup_logger_with_queue(save_data_loc: str) -> Tuple[Queue, QueueListener, s
 
     return log_queue, queue_listener, log_file
     
-def worker_logger_setup(log_queue):
+def worker_logger_setup(log_queue: multiprocessing.Queue) -> None:
+    """
+    Setup logger for worker processes using a multiprocessing queue.
+    This function configures the logger to use a QueueHandler that sends log messages
+    to a shared queue for processing by the main process.
+
+    Args:
+        log_queue (multiprocessing.Queue): Shared log queue for multiprocessing.
+
+    Returns:
+        None
+    """
     qh = logging.handlers.QueueHandler(log_queue)
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
@@ -52,6 +76,9 @@ def worker_init(log_queue: multiprocessing.Queue) -> None:
 
     Args:
         log_queue (multiprocessing.Queue): Shared log queue for multiprocessing.
+    
+    Returns:
+        None
     """
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
